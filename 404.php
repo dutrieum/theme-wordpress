f36c0de8ca856958edaaafe420ce52a89c183009
melissande <melissandec@gmail.com>
2020-10-16 11:28:54 +0100

    ecriture 404.php

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Inscrire un titre ici</title>
+    <!-- On peut avoir dautres méta-données ici -->
+  </head>
+  <body>
+    <h1>haha erreur 404</h1>
+  </body>
+</html>
+
+?>
